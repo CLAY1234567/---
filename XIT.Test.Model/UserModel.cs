236f7adbcd8cb5b21954e1bb46749af190331924
TSM-1011 <51946201+TSM-1011@users.noreply.github.com>
2019-11-05 11:10:38 +0800

    Add files via upload

---
+++ XIT.Test.Model/UserModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XIT.Test.Model
+{
+   public class UserModel
+    {
+        public int UserId { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Passwords { get; set; }
+
+        public int Sex { get; set; }
+
+
+        public int Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime Birthday { get; set; }
+
+        public int IaDelete { get; set; }
+    }
+}
